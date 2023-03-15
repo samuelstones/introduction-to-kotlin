7b459eab326acd4efd73e02b429cd9f898baa0f7
emobilis <seanorina@gmail.com>
2023-03-15 11:43:35 +0300

    kotlin project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,10 @@
 fun main(args: Array<String>) {
 
 
-
-
     var num1 = 10
     var num2 = 200
     var result = num1+num2
     print(result)
-
 
 
 }
---
+++ src/main/kotlin/datatypes.kt
@@ -0,0 +1,19 @@
+import jdk.jshell.Snippet.Status
+
+fun main(args: Array<String>) {
+    //integer or int
+    var age:Int = 30
+
+    //strings
+    var name:String = "kyle"
+    //Double
+    var temperature:Double = 40.51
+    //Boolean true or False
+    var Status = false
+    //Float
+    var point:Float = 8.5F
+    //convert double to Float
+
+    var marks = 46.587F
+
+}
