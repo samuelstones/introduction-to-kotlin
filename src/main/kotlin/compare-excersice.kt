ff575df3c095962ec88b0812177251d18c79f69b
emobilis <seanorina@gmail.com>
2023-03-15 12:05:58 +0300

    kotlin project

---
+++ src/main/kotlin/compare-excersice.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+
+    print("Enter your age")
+
+    var age: Int = readLine()!!.toInt()
+    if (age >= 18) {
+        print("Welcome to the club")
+    } else {
+
+        print("Sorry we cannot admit underage")
+
+    }
+
+
+
+    }
