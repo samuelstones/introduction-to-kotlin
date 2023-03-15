1e8d5396f1536bb837db87cdca22ef69fa41c9a9
emobilis <seanorina@gmail.com>
2023-03-15 13:45:05 +0300

    kotlin project

--- src/main/kotlin/gradingexercise.kt
+++ src/main/kotlin/gradingexercise.kt
@@ -5,26 +5,52 @@ fun main(args: Array<String>) {
     var score :Int = readLine()!!.toInt()
     print(score)
 
-    if (score >= 80) {
+    if (score in 80..100) {
         print(",Grade = A,Congratulations")
     }
 
-    else if (score <= 60) {
-        print(",Grade = B")
+    else if (score in 75..79) {
+        print(",Grade = A-")
     }
 
 
-    else if (score >= 50) {
-        print(",Grade = C")
+    else if (score in 70..74) {
+        print(",Grade = B+")
     }
 
-    else if (score >= 30) {
-        print(",Grade = D")
+    else if (score in 65..69) {
+        print(",Grade = B-")
     }
 
-    else if (score <= 20) {
-        print(",Grade = E,Repeat the exam")
+    else if (score in 60..64) {
+        print(",Grade = , C+")
     }
+
+    else if (score in 55..59) {
+        print(",Grade = , C")
+    }
+
+    else if (score in 50..54) {
+        print(",Grade = , C-")
+    }
+
+    else if (score in 45..49) {
+        print(",Grade = , D+")
+    }
+
+    else if (score in 35..44) {
+        print(",Grade = , D")
+    }
+
+    else if (score in 25..34) {
+        print(",Grade = , D-")
+    }
+
+
+    else if (score in 0..25) {
+        print(",Repeat the Exam")
+    }
+
 
 
     else {
