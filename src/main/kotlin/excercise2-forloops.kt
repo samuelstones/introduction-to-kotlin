c2445702e01f1fc4965068aa3abff7a0c0296587
emobilis <seanorina@gmail.com>
2023-03-16 13:02:08 +0300

    kotlin project

---
+++ src/main/kotlin/excercise2-forloops.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+
+    for (i in 1..100){
+        if (i % 3 == 0){
+            println("$i,Fizzbuzz")
+        }
+        else if (i % 5 == 0){
+            println("$i,Buzz")
+        }
+        else if (i % 15 == 0){
+            println("$i,Fizz")
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
