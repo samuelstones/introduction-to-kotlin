f7ae3b7d5ecdc05722ce83e8f1c7d9a4c3f8915e
emobilis <seanorina@gmail.com>
2023-03-16 12:50:20 +0300

    kotlin project

--- src/main/kotlin/BMI.kt
+++ src/main/kotlin/BMI.kt
@@ -1,6 +1,32 @@
 fun main(args: Array<String>) {
-    var weight = 80
-    var height = 1.77
-    var result = weight / height
-    print(result)
+
+    print("Enter your weight")
+    var weight :Int = readLine()!!.toInt()
+    println(weight)
+
+    print("Enter your height")
+    var height :Float = readLine()!!.toFloat()
+    println(height)
+
+
+
+    var bmi = weight / (height * height)
+    if (bmi <= 40){
+        println("underweight")
+    }
+    else if (bmi <=55){
+        println("Normal Weight")
+    }
+    else if (bmi <= 83){
+        println("overweight")
+    }
+    else{
+        println("Obese")
+    }
+
+
+
+
+
+
 }
---
+++ src/main/kotlin/Forloops.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    for (i in 1..50)
+
+        if (i % 3 == 0) {
+            println("$i is divisible ")
+        }
+
+        else{
+            println("$i NOT divisible")
+        }
+
+
+
+
+
+}
